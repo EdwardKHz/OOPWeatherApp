98f8668a1e56f4c306445fcbd552b064ce28834c
Edward Khazzoum <edwardkhazzoum@gmail.com>
2025-10-03 12:50:56 +0300

    first commit

--- src/main/java/com/finalproject/oopproject/controllers/DatabaseController.java
+++ src/main/java/com/finalproject/oopproject/controllers/DatabaseController.java
@@ -7,9 +7,9 @@ import java.sql.*;
 import java.util.List;
 
 public class DatabaseController {
-private static final String DB_URL = "jdbc:mysql://localhost:3306/weatherdb";
-private static final String USER = "root";
-private static final String PASS = "edward1234";
+private static final String DB_URL = System.getenv("DB_URL");
+private static final String USER = System.getenv("USER");
+private static final String PASS = System.getenv("PASS");
 
 public static List<String> getMatchingCities(String query) {
     ObservableList<String> results = FXCollections.observableArrayList();
